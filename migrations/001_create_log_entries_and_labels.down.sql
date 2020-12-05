3f6751c211d6e9107fd564912e56152474ea5266
momo <calvin.j.feng@gmail.com>
2020-12-04 22:58:56 -0800

    Migrated Trello API logic and migration files from playground repo to here

---
+++ migrations/001_create_log_entries_and_labels.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS log_entries;
+DROP TABLE IF EXISTS log_labels;
+DROP TABLE IF EXISTS association_log_entries_labels;
---
+++ trelloapi/definitions.go
@@ -0,0 +1,51 @@
+package trelloapi
+
+import "time"
+
+type Config struct {
+	TrelloAPIKey   string
+	TrelloAPIToken string
+}
+
+type Service interface {
+	TrelloLabelsByBoard(boardID string) (map[string]TrelloLabel, error)
+	TrelloCardsByBoard(boardID string) ([]TrelloCard, error)
+	TrelloChecklistsByBoard(boardID string) ([]TrelloChecklist, error)
+}
+
+type TrelloLabel struct {
+	ID       string `json:"id"`
+	BoardID  string `json:"idBoard"`
+	Name     string `json:"name"`
+	Color    string `json:"color"`
+	Duration time.Duration
+}
+
+type TrelloCard struct {
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	Description  string   `json:"desc"`
+	ShortID      int64    `json:"idShort"`
+	BoardID      string   `json:"idBoard"`
+	ListID       string   `json:"idList"`
+	ChecklistIDs []string `json:"idChecklists"`
+	LabelIDs     []string `json:"idLabels"`
+	IsTemplate   bool     `json:"isTemplate"`
+	Due          string   `json:"due"`
+}
+
+type TrelloChecklist struct {
+	ID       string             `json:"id"`
+	Name     string             `json:"name"`
+	CardID   string             `json:"idCard"`
+	BoardID  string             `json:"idBoard"`
+	Position int                `json:"pos"`
+	Items    []*TrelloCheckItem `json:"checkItems"`
+}
+
+type TrelloCheckItem struct {
+	ID          string `json:"id"`
+	ChecklistID string `json:"idChecklist"`
+	State       string `json:"state"`
+	Name        string `json:"name"`
+}
