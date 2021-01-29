30e151967571f1e26f71fd6e5818be0536395b7d
Calvin Feng <calvin.j.feng@gmail.com>
2021-01-29 00:45:25 -0800

    Broke out date selector

--- practicelogui/src/components/log_entry_management/LogEntryManagement.tsx
+++ practicelogui/src/components/log_entry_management/LogEntryManagement.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { LogAssignmentJSON, LogLabelJSON } from '../../shared/type_definitions'
+import { Typography } from '@material-ui/core'
+
+import { LogAssignmentJSON, LogEntryJSON, LogLabelJSON } from '../../shared/type_definitions'
+import { Paper } from '@material-ui/core'
+import DateSelector from './DateSelector'
 
 enum Mode {
   EditEntry = "EDIT_ENTRY",
@@ -7,7 +11,11 @@ enum Mode {
 }
 
 type Props = {
-
+  logLabels: LogLabelJSON[]
+  inEditLogEntry: LogEntryJSON | null
+  handleClearEditLogEntry: () => void
+  handleHTTPCreateLogEntry: (logEntry: LogEntryJSON) => void
+  handleHTTPUpdateLogEntry: (logEntry: LogEntryJSON) => void
 }
 
 type State = {
@@ -16,15 +24,105 @@ type State = {
   // Input represents value that will be submitted to backend
   // Each input state reflects the value of an input element.
   inputID: string | null
-  inputLabelID: string | null
   inputDate: Date | null
   inputDuration: number
   inputMessage: string
   inputAssignmentName: string
   inputLabelList: LogLabelJSON[]
   inputAssignmentList: LogAssignmentJSON[]
-
-  // Editing assignment is the current assignment that is being modified.
-  editingAssignment: LogAssignmentJSON | null
+  
+  // Prefix in-edit means the current entity is being modified.
+  // The assumption is that the entity already exists.
+  inEditLabelID: string | null
+  inEditAssignment: LogAssignmentJSON | null
 }
 
+const defaultState: State = {
+  mode: Mode.NewEntry,
+  inputID: null,
+  inputDate: new Date(),
+  inputDuration: 0,
+  inputMessage: "",
+  inputAssignmentName: "",
+  inputLabelList: [],
+  inputAssignmentList: [],
+  inEditLabelID: null,
+  inEditAssignment: null
+}
+
+export default class LogEntryManagement extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    if (props.inEditLogEntry == null) {
+      this.state = defaultState
+    } else {
+      // If inEditLogEntry has a nil ID, we are creating a new entry.
+      let mode: Mode = Mode.EditEntry
+      if (props.inEditLogEntry.id.length === 0) {
+        mode = Mode.NewEntry
+      }
+
+      this.state = {
+        mode: mode,
+        inputID: props.inEditLogEntry.id,
+        inputDate: props.inEditLogEntry.date,
+        inputDuration: props.inEditLogEntry.duration,
+        inputMessage: props.inEditLogEntry.message,
+        inputAssignmentName: "",
+        inputLabelList: props.inEditLogEntry.labels,
+        inputAssignmentList: props.inEditLogEntry.assignments,
+        inEditLabelID: null,
+        inEditAssignment: null
+      }
+    }
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.inEditLogEntry === null) {
+      this.setState(defaultState)
+      return
+    }
+
+    let mode: Mode = Mode.EditEntry
+    if (nextProps.inEditLogEntry.id.length === 0) {
+      mode = Mode.NewEntry
+    }
+
+    this.setState({
+      mode: mode,
+      inputID: nextProps.inEditLogEntry.id,
+      inputDate: nextProps.inEditLogEntry.date,
+      inputDuration: nextProps.inEditLogEntry.duration,
+      inputMessage: nextProps.inEditLogEntry.message,
+      inputAssignmentName: "",
+      inputLabelList: nextProps.inEditLogEntry.labels,
+      inputAssignmentList: nextProps.inEditLogEntry.assignments,
+      inEditLabelID: null,
+      inEditAssignment: null
+    })
+  }
+
+  handleSetInputDate = (date: Date | null) => {
+    this.setState({ inputDate: date })
+  }
+
+  get header() {
+    switch(this.state.mode) {
+      case Mode.EditEntry:
+        return <Typography variant="h5">Edit Log Entry {this.state.inputID}</Typography>
+      case Mode.NewEntry:
+        return <Typography variant="h5">Add Log Entry</Typography>  
+    }
+  }
+
+  render() {
+    return (
+      <Paper className="LogEntryManagement">
+        {this.header}
+        <DateSelector
+          inputDate={this.state.inputDate}
+          setInputDate={this.handleSetInputDate} />
+      </Paper>
+    )
+  }
+}
