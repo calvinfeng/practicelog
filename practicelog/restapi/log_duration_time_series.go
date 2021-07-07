da4308a555c044fcad3eb09806839b081795f28a
Calvin Feng <calvin.j.feng@gmail.com>
2021-07-07 13:27:03 -0700

    Refactored time series generation. I broke them into multiple helper functions

--- practicelog/restapi/log_duration_time_series.go
+++ practicelog/restapi/log_duration_time_series.go
@@ -35,26 +35,10 @@ func (s *server) GetLogEntryDurationSum(c echo.Context) error {
 }
 
 func (s *server) GetLogEntryDurationTimeSeries(c echo.Context) error {
-	var inSameBucket func(timeSeriesDataPoint, *practicelog.Entry) bool
 	grouping := group(c.QueryParam("group"))
-
-	switch grouping {
-	case byDay:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year() &&
-				prev.Month == entry.Date.Month() &&
-				prev.Day == entry.Date.Day()
-		}
-	case byMonth:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year() && prev.Month == entry.Date.Month()
-		}
-	case byYear:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year()
-		}
-	default:
-		return echo.NewHTTPError(http.StatusBadRequest, "query parameter ?group=[by_day, by_month, by_year] is required")
+	if grouping != byDay && grouping != byMonth && grouping != byYear {
+		return echo.NewHTTPError(http.StatusBadRequest,
+			fmt.Sprintf("query parameter is required ?group=[by_day, by_month, by_year]"))
 	}
 
 	count, err := s.store.CountLogEntries()
@@ -69,40 +53,7 @@ func (s *server) GetLogEntryDurationTimeSeries(c echo.Context) error {
 			errors.Wrap(err, "failed to query database for log entries").Error())
 	}
 
-	// TODO: Refactor the bucketing strategy
-
-	timeSeries := make([]timeSeriesDataPoint, 0)
-	var accum int32
-	for i := len(entries) - 1; i >= 0; i-- {
-		if len(timeSeries) > 0 {
-			lastEl := timeSeries[len(timeSeries)-1]
-			if inSameBucket(lastEl, entries[i]) {
-				accum += entries[i].Duration
-				timeSeries[len(timeSeries)-1].Value = float32(accum)
-				continue
-			}
-		}
-		accum = entries[i].Duration
-
-		year, month, day := entries[i].Date.Date()
-		newDataPoint := timeSeriesDataPoint{Value: float32(accum)}
-
-		switch grouping {
-		case byDay:
-			newDataPoint.Day = day
-			newDataPoint.Month = month
-			newDataPoint.Year = year
-			newDataPoint.Key = fmt.Sprintf("%d %s %02d", year, month.String(), day)
-		case byMonth:
-			newDataPoint.Month = entries[i].Date.Month()
-			newDataPoint.Year = entries[i].Date.Year()
-			newDataPoint.Key = fmt.Sprintf("%d %s", year, month.String())
-		case byYear:
-			newDataPoint.Year = entries[i].Date.Year()
-			newDataPoint.Key = fmt.Sprintf("%d", year)
-		}
-		timeSeries = append(timeSeries, newDataPoint)
-	}
+	timeSeries := generateTimeSeries(entries, grouping, false)
 
 	return c.JSON(http.StatusOK, durationTimeSeriesJSONResponse{
 		Count:      count,
@@ -112,26 +63,10 @@ func (s *server) GetLogEntryDurationTimeSeries(c echo.Context) error {
 }
 
 func (s *server) GetLogEntryDurationCumulativeSumTimeSeries(c echo.Context) error {
-	var inSameBucket func(timeSeriesDataPoint, *practicelog.Entry) bool
 	grouping := group(c.QueryParam("group"))
-
-	switch grouping {
-	case byDay:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year() &&
-				prev.Month == entry.Date.Month() &&
-				prev.Day == entry.Date.Day()
-		}
-	case byMonth:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year() && prev.Month == entry.Date.Month()
-		}
-	case byYear:
-		inSameBucket = func(prev timeSeriesDataPoint, entry *practicelog.Entry) bool {
-			return prev.Year == entry.Date.Year()
-		}
-	default:
-		return echo.NewHTTPError(http.StatusBadRequest, "query parameter ?group=[by_day, by_month, by_year] is required")
+	if grouping != byDay && grouping != byMonth && grouping != byYear {
+		return echo.NewHTTPError(http.StatusBadRequest,
+			fmt.Sprintf("query parameter is required ?group=[by_day, by_month, by_year]"))
 	}
 
 	count, err := s.store.CountLogEntries()
@@ -146,23 +81,54 @@ func (s *server) GetLogEntryDurationCumulativeSumTimeSeries(c echo.Context) erro
 			errors.Wrap(err, "failed to query database for log entries").Error())
 	}
 
-	// Entries are in descending order by date. The only thing I need to change is the map key for by day, by month,
-	// or by year.
+	// Convert minutes to hours for this particular API
+	timeSeries := generateTimeSeries(entries, grouping, true)
+	for i := range timeSeries {
+		timeSeries[i].Value /= 60
+	}
+
+	return c.JSON(http.StatusOK, durationTimeSeriesJSONResponse{
+		Count:      count,
+		TimeSeries: timeSeries,
+		Group:      grouping,
+	})
+}
+
+func inSameGroup(prev timeSeriesDataPoint, curr *practicelog.Entry, grouping group) bool {
+	switch grouping {
+	case byDay:
+		return prev.Year == curr.Date.Year() && prev.Month == curr.Date.Month() && prev.Day == curr.Date.Day()
+	case byMonth:
+		return prev.Year == curr.Date.Year() && prev.Month == curr.Date.Month()
+	case byYear:
+		return prev.Year == curr.Date.Year()
+	default:
+		panic("grouping strategy must be one of [by day, by month, by year]")
+	}
+}
+
+func generateTimeSeries(entries []*practicelog.Entry, grouping group, cumulative bool) []timeSeriesDataPoint {
 	timeSeries := make([]timeSeriesDataPoint, 0)
-	var accum int32 = 0
+
+	var accum int32
+	// Entries are in descending order by date.
 	for i := len(entries) - 1; i >= 0; i-- {
 		accum += entries[i].Duration
 
-		if len(timeSeries) > 0 {
-			lastEl := timeSeries[len(timeSeries)-1]
-			if inSameBucket(lastEl, entries[i]) {
-				timeSeries[len(timeSeries)-1].Value = float32(accum) / 60
-				continue
-			}
+		if len(timeSeries) > 0 && inSameGroup(timeSeries[len(timeSeries)-1], entries[i], grouping) {
+			timeSeries[len(timeSeries)-1].Value += float32(entries[i].Duration)
+			continue
 		}
 
 		year, month, day := entries[i].Date.Date()
-		newDataPoint := timeSeriesDataPoint{Value: float32(accum) / 60}
+
+		var newDataPoint timeSeriesDataPoint
+		if cumulative {
+			newDataPoint = timeSeriesDataPoint{Value: float32(accum)}
+		} else {
+			newDataPoint = timeSeriesDataPoint{Value: float32(entries[i].Duration)}
+		}
+
 		switch grouping {
 		case byDay:
 			newDataPoint.Day = day
@@ -179,10 +145,5 @@ func (s *server) GetLogEntryDurationCumulativeSumTimeSeries(c echo.Context) erro
 		}
 		timeSeries = append(timeSeries, newDataPoint)
 	}
-
-	return c.JSON(http.StatusOK, durationTimeSeriesJSONResponse{
-		Count:      count,
-		TimeSeries: timeSeries,
-		Group:      grouping,
-	})
+	return timeSeries
 }
