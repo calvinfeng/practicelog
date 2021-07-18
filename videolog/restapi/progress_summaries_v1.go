e483a660845592a5c389f06b7904a95b53f497b6
Calvin Feng <calvin.j.feng@gmail.com>
2021-07-17 23:11:59 -0700

    Implemented a V1 version of updating progress summary, easier for me to edit

--- videolog/restapi/progress_summaries_v1.go
+++ videolog/restapi/progress_summaries_v1.go
@@ -1,6 +1,9 @@
 package restapi
 
 import (
+	"github.com/calvinfeng/practicelog/auth"
+	"github.com/calvinfeng/practicelog/videolog"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
 	"net/http"
@@ -17,5 +20,38 @@ func (api *apiV1) ListProgressSummaries(c echo.Context) error {
 }
 
 func (api *apiV1) UpsertProgressSummary(c echo.Context) error {
-	panic("implement me")
+	email, err := auth.GetEmailFromContext(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "email is not found in provided ID token")
+	}
+
+	summary := new(videolog.ProgressSummary)
+	if err := c.Bind(summary); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest,
+			errors.Wrap(err, "failed to parse JSON data").Error())
+	}
+	summary.Username = email
+
+	if c.Param("summary_id") == "" {
+		if err := api.videoLogStore.UpsertProgressSummaries(summary); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				errors.Wrapf(err, "failed to create %d-%d progress summary", summary.Year, summary.Month))
+		}
+		return c.JSON(http.StatusCreated, IDResponse{ID: summary.ID})
+	} else {
+		if id, err := uuid.Parse(c.Param("summary_id")); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest,
+				errors.Wrap(err, "summary id in path parameter is not a valid uuid ").Error())
+		} else {
+			if id != summary.ID {
+				return echo.NewHTTPError(http.StatusBadRequest,
+					"payload summary ID does not match with path summary ID")
+			}
+		}
+		if err := api.videoLogStore.UpsertProgressSummaries(summary); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError,
+				errors.Wrapf(err, "failed to update %d-%d progress summary", summary.Year, summary.Month))
+		}
+		return c.JSON(http.StatusOK, summary)
+	}
 }
